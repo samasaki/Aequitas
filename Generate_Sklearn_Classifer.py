fa7906332e1ef0c121947853ab1d06ad41773dfd
Sousuke Amasaki <amasaki@cse.oka-pu.ac.jp>
2022-07-10 17:17:51 +0900

    Reproduced unfair models.

--- Generate_Sklearn_Classifer.py
+++ Generate_Sklearn_Classifer.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn import svm
-from sklearn.externals import joblib
+import joblib
 from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
@@ -39,11 +39,23 @@ X = np.array(X)
 Y = np.array(Y)
 print(neg_count, pos_count)
 
-# w = svm.SVC(gamma=0.0025)
+model_svm = svm.SVC(gamma=0.0025)
+model_svm.fit(X, Y)
+print('SVM:', cross_val_score(model_svm, X, Y, scoring='accuracy'))
+joblib.dump(model_svm, 'SVM_standard_unfair.pkl', compress=3)
 
-# model = MLPClassifier(solver='lbfgs', alpha=1e-5,
-#                       hidden_layer_sizes=(7, 5), random_state=1)
-model = DecisionTreeClassifier()
-model.fit(X, Y)
-print(cross_val_score(model, X, Y, scoring='accuracy'))
-joblib.dump(model, 'Decision_tree_standard_unfair.pkl')
+model_mlp = MLPClassifier(solver='lbfgs', alpha=1e-5,
+                        hidden_layer_sizes=(7, 5), random_state=1, max_iter=20000)
+model_mlp.fit(X, Y)
+print('MLPC:', cross_val_score(model_mlp, X, Y, scoring='accuracy'))
+joblib.dump(model_mlp, 'MLPC_standard_unfair.pkl', compress=3)
+
+model_dt = DecisionTreeClassifier()
+model_dt.fit(X, Y)
+print('DT:', cross_val_score(model_dt, X, Y, scoring='accuracy'))
+joblib.dump(model_dt, 'Decision_tree_standard_unfair.pkl', compress=3)
+
+model_rf = RandomForestClassifier()
+model_rf.fit(X, Y)
+print('RF:', cross_val_score(model_rf, X, Y, scoring='accuracy'))
+joblib.dump(model_rf, 'Random_Forest_standard_unfair.pkl', compress=3)
